6d0c10ec30fc0611f0cf5183232ac19e1824bf38
test01 <66986661+testweb01@users.noreply.github.com>
2023-12-13 22:37:16 +0530

    Update commit.bash

--- script/commit.bash
+++ script/commit.bash
@@ -1,6 +1,6 @@
 echo 'git config'
-git config --local user.email "testwebapplication01@gmail.com"
-git config --global user.name "@0xweb7"
+git config --local user.email "web705207@gmail.com"
+git config --global user.name "0xweb7"
 date > date.txt
 git add date.txt
 git commit -m "using blank yml"
