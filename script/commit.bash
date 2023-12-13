e8f527f202f472a222b3fb7157baf7673cc7b45c
test01 <66986661+testweb01@users.noreply.github.com>
2023-12-13 22:33:12 +0530

    Create commit.bash

---
+++ script/commit.bash
@@ -0,0 +1,7 @@
+echo 'git config'
+git config --local user.email "testwebapplication01@gmail.com"
+git config --global user.name "@0xweb7"
+date > date.txt
+git add date.txt
+git commit -m "using blank yml"
+git push
